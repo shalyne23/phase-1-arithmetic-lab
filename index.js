d381ec2b12adcd48a386f5b9944204e4092ceedc
shalyne23 <shalynelangat@gmail.com>
2024-07-01 17:07:29 +0300

    updated

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 
-// Write your code here
+const num1=2;
+const num2=31;
+
+const multiply=Number(num1)*Number(num2);
+console.log(multiply)
+
+
+const random = parseInt(Math.random(2, 1000)*100)
+console.log(random)
+const num3=28
+const num4=6
+const mod = num3 % num4
+
+
+let setss=[0,20,10,12,19]
+let nums='';
+
+console.log(...setss)
+
+let max = Math.max(...setss)
+
+console.log(max)
